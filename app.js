55d0b362dc2bde215a4c759a7634afca2f4bdd11
Ana Beatriz <anabeatrizaraujolopes2004@gmail.com>
2025-03-18 21:45:10 -0300

    adcionando primeira função

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeAmigos = []; 
+
+function adicionarAmigo() {
+    let nome = document.getElementById("amigo").value;
+    if(nome === "") {
+    } else {
+        listaDeAmigos.push(nome); //Adciona o nome a lista 
+        console.log(listaDeAmigos);
+    }
+}
